ce82a82f4d91c457dc3e03b05fbcedb0d3e3cf38
Ajurgs <ajjurgs5954@gmail.com>
2021-04-18 17:05:09 -0600

    added lenght slider to the modal

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lengthSlider = document.getElementById("length");
+var lengthValue = document.getElementById("value");
+
+lengthSlider.oninput = function () {
+  lengthValue.innerHTML = this.value;
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function passwordCriteria() {}
+function generatePassword() {}
